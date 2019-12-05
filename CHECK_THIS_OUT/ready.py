1c63169d09d5079547ba04ec77be651a67a7ce3f
Arek Michalski <arekmichalski@icloud.com>
2019-12-05 02:40:48 +0100

    CHECK_THIS_OUT

---
+++ CHECK_THIS_OUT/ready.py
@@ -0,0 +1,85 @@
+#python ready.py 1.jpg
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+import sys
+from PIL import Image
+
+var = sys.argv[1]
+
+# load the example image and convert it to grayscale
+img = cv2.imread(var)
+
+img = cv2.resize(img, (620,480) )
+#cv2.imshow('1', img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
+#cv2.imshow('2', gray)
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+#cv2.imshow('3', gray)
+edged = cv2.Canny(gray, 50, 150, apertureSize=3) #Perform Edge detection
+#cv2.imshow('4', edged)
+# find contours in the edged image, keep only the largest
+# ones, and initialize our screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+# loop over our contours
+for c in cnts:
+ # approximate the contour
+ peri = cv2.arcLength(c, True)
+ approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+ 
+ # if our approximated contour has four points, then
+ # we can assume that we have found our screen
+ if len(approx) == 4:
+  screenCnt = approx
+  break
+
+ 
+
+if screenCnt is None:
+ detected = 0
+ print "No contour detected"
+else:
+ detected = 1
+ cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
+
+# Masking the part other than the number plate
+mask = np.zeros(gray.shape,np.uint8)
+new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+new_image = cv2.bitwise_and(img,img,mask=mask)
+
+# Now crop
+(x, y) = np.where(mask == 255)
+(topx, topy) = (np.min(x), np.min(y))
+(bottomx, bottomy) = (np.max(x), np.max(y))
+Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+cv2.imshow('cropped', Cropped)
+
+threshGauss = cv2.adaptiveThreshold(border, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 27)
+cv2.imshow('tv1', threshGauss)
+#threshGauss = cv2.medianBlur(threshGauss, 3)
+#cv2.imshow('tv2', threshGauss)
+
+# OCR
+config = '-l eng --oem 1 --psm 3'
+text = pytesseract.image_to_string(threshGauss, config=config)
+
+validChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+cleanText = []
+
+for char in text:
+    if char in validChars:
+        cleanText.append(char)
+
+plate = ''.join(cleanText)
+
+print(plate)
+
+cv2.imshow('Cropped',img)
+
+cv2.waitKey(0)
