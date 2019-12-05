56a66077046796411020e21c74aaf16ecd03940d
amichalsky <arekmichalski@icloud.com>
2019-12-05 15:39:36 +0100

    Update Ocr.py

--- Ocr.py
+++ Ocr.py
@@ -9,7 +9,8 @@ from PIL import Image
 # OCR
 config = '-l eng --oem 1 --psm 3'
 def ocr (croppedImage):
-    text = pytesseract.image_to_string(croppedImage, config=config)
+    threshGauss = cv2.adaptiveThreshold(croppedImage, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 27)
+    text = pytesseract.image_to_string(threshGauss, config=config)
 
     validChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
